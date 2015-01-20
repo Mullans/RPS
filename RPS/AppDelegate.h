060d58a88b2df91eb72fc21de30dd291620ad17a
Sean Mullan <seanmm@mchsi.com>
2015-01-19 21:37:21 -0600

    Initial Commit

---
+++ RPS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RPS
+//
+//  Created by Sean Mullan on 1/19/15.
+//  Copyright (c) 2015 SilentLupin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RPS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RPS
+//
+//  Created by Sean Mullan on 1/19/15.
+//  Copyright (c) 2015 SilentLupin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
